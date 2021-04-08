f03c9152fe2f38bcbb530343ba14b6db3d7f68dd
extremq <45830561+extremq@users.noreply.github.com>
2021-04-08 23:41:51 +0300

    Create problem9.py

---
+++ problem9.py
@@ -0,0 +1,10 @@
+# a < b < c, so the max triplet is 1, 499, 500 for which a + b + c = 1000
+def compute():
+    for b in range(1, 499):
+        for a in range(1, b):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                return a * b * c
+
+
+print(compute())
