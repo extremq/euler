ed6958b72d75ec174a5f9fc8ca060c82b7dfed84
extremq <45830561+extremq@users.noreply.github.com>
2021-04-09 11:05:31 +0300

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -5,10 +5,10 @@ def primeness_sieve(n):
     result = [True] * (n + 1)
     result[0] = False
     result[1] = False
-    for i in range(2, n):
+    for i in range(2, n + 1):
         if result[i]:
             primes.append(i)
-            for j in range(i ** 2, n, i):
+            for j in range(i ** 2, n + 1, i):
                 result[j] = False
 
     return result
